765ec3438343bda91796006a8dabf7e35e7ac87a
Francesco Arcuri <farcuri@ucr.edu>
2024-02-23 16:16:10 -0800

    startTest_MySQL.py complete

--- testFile/pyTest/startTest_MySQL.py
+++ testFile/pyTest/startTest_MySQL.py
@@ -2,7 +2,7 @@ import time
 from testFile.pyTest.testSDU_MySQL import runTest
 from testFile.tools import saveResult, estimate_completion_time, print_progress_bar, string_time, save_info
 
-numberOfTests = 5000000
+numberOfTests = 1000
 
 directory = "../testResult"  # Directory to save result
 
@@ -33,25 +33,25 @@ save_info(directory + info_file)
 # Phase 1 Tests time without Middleware
 start = time.perf_counter()
 
-# all_test_noProxy = []
-#
-# for i in range(numberOfTests):
-#     test_execution = runTest(3306)
-#     if len(test_execution) != 5: raise ValueError("List elements != 5")
-#     all_test_noProxy.append(test_execution)
-#     if i % 100 == 0:
-#         print("noProxy:" + str(i))
-#         remaining_time = estimate_completion_time(numberOfTests + numberOfTests - i, time.perf_counter()-start, i)
-#         print("remaining_time: " + string_time(remaining_time))
-#         print('********************')
-#         print_progress_bar(2*numberOfTests, i)
-#         print('\n********************')
-#
-# print("start saving result_noProxy_")
-# saveResult(directory, all_test_noProxy, "result_noProxy_" + str(numberOfTests) + ".csv")
-# print("saving finished result_noProxy_")
-# time.sleep(5)
-# start += 5
+all_test_noProxy = []
+
+for i in range(numberOfTests):
+    test_execution = runTest(3306)
+    if len(test_execution) != 5: raise ValueError("List elements != 5")
+    all_test_noProxy.append(test_execution)
+    if i % 100 == 0:
+        print("noProxy:" + str(i))
+        remaining_time = estimate_completion_time(numberOfTests + numberOfTests - i, time.perf_counter()-start, i)
+        print("remaining_time: " + string_time(remaining_time))
+        print('********************')
+        print_progress_bar(2*numberOfTests, i)
+        print('\n********************')
+
+print("start saving result_noProxy_")
+saveResult(directory, all_test_noProxy, "result_noProxy_" + str(numberOfTests) + ".csv")
+print("saving finished result_noProxy_")
+time.sleep(5)
+start += 5
 
 # Phase 2 Tests time with Middleware
 all_test_Proxy = []  # Matrix with all test execution
